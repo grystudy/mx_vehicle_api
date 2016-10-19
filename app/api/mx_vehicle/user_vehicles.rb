ff14c727eedc49a193594f237dba2c4d65732df1
yangy <yangy@meixing.com>
2016-10-19 09:54:13 +0800

    自动生成api文档

--- app/api/mx_vehicle/user_vehicles.rb
+++ app/api/mx_vehicle/user_vehicles.rb
@@ -1,11 +1,19 @@
 class MxVehicle::UserVehicles < Grape::API
-	namespace :UserVehicle do
+  params do
+    requires :user_id, type: Integer, desc: 'user ID.'
+  end
+  namespace :UserVehicle do
     desc "Get all vehicles of this user."
-    params do
-      requires :user_id, type: Integer, desc: 'user ID.'
-    end   
     get 'all' do
       [{sb: 250}]
+    end
+
+    desc 'Create a vehicle.'
+    params do
+      requires :chepai, type: String, desc: 'Plate number of the car.'
+    end
+    post 'Create' do
+      {'declared_params' => declared(params)}
     end
   end
 end
