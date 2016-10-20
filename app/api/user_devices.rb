b38a7315824f066cf4543e0ddef23e7df322f6bc
yangy <yangy@meixing.com>
2016-10-20 18:00:49 +0800

    hide doc

--- app/api/user_devices.rb
+++ app/api/user_devices.rb
@@ -7,7 +7,7 @@ module API
       params do
         requires :user, type: Integer, desc: '用户Id'
         requires :device, type: String, desc: '设备Id'
-        requires :status, type: Integer, values: [1, 2], desc: '0:关 1：开'
+        requires :status, type: Integer, values: [0, 1], desc: '0:关 1：开'
       end
       get "switch" do
         devices = UserDevice.where(user: params[:user],device: params[:device]).all
