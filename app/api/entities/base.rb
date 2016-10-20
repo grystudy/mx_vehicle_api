4d39fd39297b00a9989250a28733b3e63953c4b2
yangy <yangy@meixing.com>
2016-10-20 18:18:29 +0800

    t

--- app/api/entities/base.rb
+++ app/api/entities/base.rb
@@ -2,7 +2,7 @@ Dir["#{Rails.root}/app/api/entities/*.rb"].each { |file| require file }
 module API
   module Entities
     class Base < Grape::Entity
-      include API::Entities::Defaults
+      # include API::Entities::Defaults
     end
   end
 end
