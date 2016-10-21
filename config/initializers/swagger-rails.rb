5d03c393e5dfbaf3209ab590db3bfa8145c98340
yangy <yangy@meixing.com>
2016-10-21 09:08:05 +0800

    add swagger config

--- config/initializers/swagger-rails.rb
+++ config/initializers/swagger-rails.rb
@@ -1,2 +1,5 @@
 GrapeSwaggerRails.options.url      = "/api/v1/swagger_doc"
-GrapeSwaggerRails.options.app_url  = '/'
+#GrapeSwaggerRails.options.app_url  = '/'
+GrapeSwaggerRails.options.before_filter_proc = proc {
+  GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
+}
