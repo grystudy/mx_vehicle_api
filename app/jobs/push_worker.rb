49bdd6a5cf1c4afce839b3f172c5db75d3f4ef76
yangy <yangy@meixing.com>
2016-11-03 17:38:56 +0800

    add sidetiq

--- app/jobs/push_worker.rb
+++ app/jobs/push_worker.rb
@@ -2,9 +2,13 @@ class PushWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence {secondly(30) }
+  recurrence {secondly(1) }
 
   def perform
     puts "sidetip ttt"
   end
+end
+
+module AA
+  PushWorker.perform_async
 end
--- config/initializers/swagger-rails.rb
+++ config/initializers/swagger-rails.rb
@@ -1,6 +1,6 @@
 GrapeSwaggerRails.options.url      = "/api/v1/swagger_doc"
-#GrapeSwaggerRails.options.app_url  = '/'
+GrapeSwaggerRails.options.app_url  = '/'
 # GrapeSwaggerRails.options.before_filter_proc = proc {
 #   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
 # }
-GrapeSwaggerRails.options.app_url  = "#{ENV['APPLICATION_URL']}"
+# GrapeSwaggerRails.options.app_url  = "#{ENV['APPLICATION_URL']}"
